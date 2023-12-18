76fb3b88216e859e6dd10c16b034109074f9a0c3
samuel farias <samuelcontatoe1@gmail.com>
2023-12-18 16:32:17 -0300

    lição finalizada

---
+++ js/app.js
@@ -0,0 +1,51 @@
+/*
+const name = "sam";
+const text = `Hello, ${name}`;
+console.log(text);
+
+const one = "Hello, ";
+const two = "como vc está?";
+const juncao = `${one}${two}`;
+console.log(juncao);
+
+const song = "music";
+const score = 9;
+const gscore = 10;
+const output = `i gosto da musica ${song}. eu já ouvi ela ${(score/gscore)*100}%.`
+console.log(output);
+
+let l = song.length;
+console.log(l);
+*/
+
+//let text = "banana, maca, limao";
+//let part = text.substr(-4);
+//console.log(part);
+
+/*
+let text = "eu visitei a microsoft";
+let newText = text.replace("microsoft", "toyota");
+console.log(newText);
+
+let text1 = "Hello";
+let text2 = "World";
+let text3 = text1.concat(" ", text2);
+console.log(text3);
+*/
+//trim remove espaços em branco
+
+/*
+let text = "5";
+let padded = text.padStart(4,"0");
+console.log(padded);
+
+*/
+
+/**
+ * ===== Troubleshooting =====
+ * The function below should log the number 2, however it does not, 
+ * see if you can fix it!
+ * Be sure to fix it in the spirit of the code, do not hard code the result.
+ */
+
+
